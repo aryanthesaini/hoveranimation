89da0d2b0d268096b7c07a3803302dab5bf7d5ab
aryanthesaini <68115113+aryanthesaini@users.noreply.github.com>
2022-01-11 15:12:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    angle: Math.PI / 8,
+    speedIn: 2,
+    speedOut: 3,
+    image1: 'https://raw.githubusercontent.com/aryanthesaini/hoveranimation/main/picmain.png',
+    image2: 'https://raw.githubusercontent.com/aryanthesaini/hoveranimation/main/picmain2.png',
+    displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/ramen.jpg'
+
+})
